688e58112381a97bd233fe78f8b9dd6e3b0a08ea
oya10 <32030081+oya10@users.noreply.github.com>
2018-12-19 16:49:37 +0800

    Update read_nmap.py

--- read_nmap.py
+++ read_nmap.py
@@ -2,11 +2,14 @@ import re
 
 filename = "nmap0-test_127.0.0.1"
 
-ports_ssl = []
-ports_smb = []
-ports_http = []
+#ports_ssl = []
+#ports_smb = []
+#ports_http = []
 
 def reader(filename):
+    ports_ssl = []
+    ports_smb = []
+    ports_http = []
     with open(filename,'r') as nmap_out:
         line = nmap_out.readline()
         while line != "":
@@ -23,15 +26,16 @@ def reader(filename):
                     ports_smb.append(int(line.split('/')[0]))
                 if re.findall("http",port_service):
                     ports_http.append(int(line.split('/')[0]))
-#               print (this_line[3]) //service information in 4th element
-#               for i in this_line:
-#                   print (i)
             line = nmap_out.readline()
         pass
-#    print (ports_ssl)
-#    print (ports_smb)
-#    print (ports_http)
     print ("reading nmap ports... done.")
+    return (ports_ssl,ports_smb,ports_http)
+
+def clear_all_lists():
+    ports_ssl = []
+    ports_smb = []
+    ports_http = []
+    pass
 
 def output_all():
     return (ports_ssl,ports_smb,ports_http)
