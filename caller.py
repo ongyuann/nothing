14be99057c3e4eecd82f959007ed9c2d7953be51
oya10 <32030081+oya10@users.noreply.github.com>
2018-12-19 16:48:57 +0800

    Update caller.py

--- caller.py
+++ caller.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-import os,time
+import os,sys,time,subprocess,shlex,re
 import read_nmap as rn
 '''
 setting up
@@ -11,7 +11,10 @@ print("*************************************************************************
 print("\nEnter the text files that contains the IP addresses (if more than 1 list, please separate by comma, e.g. list1,list2)")
 lists = str(raw_input("list: "))
 print("\nPlease enter time delay till scan starts (in minutes)")
-wait = int(raw_input("time in mins: "))
+try:
+    wait = int(raw_input("time in mins: "))
+except:
+    wait = 0
 wait = wait*60
 
 lists = lists.split(',')
@@ -23,7 +26,7 @@ def check_files(each_list_of_files):
     try:
         file = open(each_list_of_files,'r')
         return True
-    except FileNotFoundError:
+    except:
         print (each_list_of_files + " not found. quitting.")
 
 def action_on_files(list_of_files):
@@ -49,43 +52,78 @@ def action_on_list(curr_list):
     pass
 
 '''
+special runner + writer function
+'''
+def run_command(command,name_of_output):
+    process = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    try:
+        output_file = open(name_of_output,'r')
+        os.system('mv '+name_of_output+' '+name_of_output+'.old')
+        output_file.close()
+    except:
+        os.system('touch '+name_of_output)
+    while 1:
+        shell_output = process.stdout.readline().rstrip()
+        if re.findall('nmap|testssl',command) and shell_output == "" and process.poll() is not None:
+            break
+        if shell_output != "":
+            print (shell_output)
+            os.system('echo "'+shell_output+'" >> '+name_of_output)
+            if re.findall("testssl",command) and re.findall("doesn't seem to be a",shell_output):
+                process.stdin.write("yes")
+                process.stdin.close()
+    try:
+        rc = process.communicate()
+    except:
+        rc = process.poll()
+    #rc = process.poll()
+    return rc
+
+'''
 script 'runners' below
 '''
 def run_nmap_tcp(ip_add_of_focus,curr_list):
     #print(ip_add_of_focus)
     ip_add_of_focus = ip_add_of_focus.rstrip()
-    nmap_output = curr_list+"_nmap_tcp_"+ip_add_of_focus
-    nmap_output = nmap_output.rstrip()
-    nmap_cmd = "nmap -sS -sV -oN "+nmap_output+" "+ip_add_of_focus
-    print ("running nmap ...")
-    os.system(nmap_cmd)
-    rn.reader(nmap_output)
-    list_of_ssl_ports = rn.output_ssl()
+    name_of_output = curr_list+"_nmap_tcp_"+ip_add_of_focus
+    name_of_output = name_of_output.rstrip()
+    cmd = "nmap -v -sS -sC -sV "+ip_add_of_focus
+    print ("[*]running nmap ...")
+    run_command(cmd,name_of_output)
+    list_of_ssl_ports = rn.reader(name_of_output)[0]
+    #list_of_ssl_ports = rn.output_ssl()
+    #print ("[*]BREAK")
     #print (list_of_ssl_ports)
+    #sys.exit()
     run_sslscan(ip_add_of_focus,list_of_ssl_ports,curr_list)
     run_testssl(ip_add_of_focus,list_of_ssl_ports,curr_list)
     pass
 
 def run_sslscan(ip_add_of_focus,list_of_ssl_ports,curr_list):
-    #print("hello")
     ip_add_of_focus = ip_add_of_focus.rstrip()
-    cmd = "sslscan " +ip_add_of_focus+":"
     for ssl_port in list_of_ssl_ports:
-        print ("running sslscan on " + ip_add_of_focus + " on port " + str(ssl_port))
+        print ("[*]running sslscan on " + ip_add_of_focus + " on port " + str(ssl_port))
+        cmd = "sslscan "+ip_add_of_focus+":"
         cmd += str(ssl_port)
-        cmd += " > "+curr_list+"_sslscan_"+ip_add_of_focus
-        os.system(cmd)
+        name_of_output = curr_list+"_sslscan_"+ip_add_of_focus+":"+str(ssl_port)
+        #run_command(cmd,name_of_output)
+        try:
+            output_file = open(name_of_output,'r')
+            os.system('mv '+name_of_output+' '+name_of_output+'.old')
+            output_file.close()
+        except:
+            os.system('touch '+name_of_output)
+        os.system(cmd+" >> "+name_of_output)
     pass
 
 def run_testssl(ip_add_of_focus,list_of_ssl_ports,curr_list):
-    #print("hello testssl here")
     ip_add_of_focus = ip_add_of_focus.rstrip()
-    cmd = "/root/Desktop/tools/scripts/testssl.sh/testssl.sh " +ip_add_of_focus+":"
     for ssl_port in list_of_ssl_ports:
-        print ("running testssl on " + ip_add_of_focus + " on port " + str(ssl_port))
+        print ("[*]running testssl on " + ip_add_of_focus + " on port " + str(ssl_port))
+        cmd = "/root/Desktop/tools/scripts/testssl.sh/testssl.sh " +ip_add_of_focus+":"
         cmd += str(ssl_port)
-        cmd += " > "+curr_list+"_testssl_"+ip_add_of_focus
-        os.system(cmd)
+        name_of_output = curr_list+"_testssl_"+ip_add_of_focus+":"+str(ssl_port)
+        run_command(cmd,name_of_output)
     pass
 
 '''
