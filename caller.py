c179a22a8a85d4f4b3f9f596d81fab65b2dc5c8f
oya10 <32030081+oya10@users.noreply.github.com>
2019-01-17 17:24:13 +0800

    Update caller.py

--- caller.py
+++ caller.py
@@ -182,7 +182,10 @@ def run_sslscan(ip_add_of_focus,list_of_ssl_ports,curr_list,folder_name):
     for ssl_port in list_of_ssl_ports:
         print ("[*]running sslscan on " + ip_add_of_focus + " on port " + str(ssl_port))
         #ssl --no-color
-        cmd = "sslscan "+ip_add_of_focus+":"
+        if want_colour:
+            cmd = "sslscan --no-color "+ip_add_of_focus+":"
+        else:
+            cmd = "sslscan "+ip_add_of_focus+":"
         cmd += str(ssl_port)
         name_of_output = folder_name+"/"+curr_list+"_sslscan_"+ip_add_of_focus+"-"+str(ssl_port)
         #run_command(cmd,name_of_output)
@@ -200,7 +203,10 @@ def run_testssl(ip_add_of_focus,list_of_ssl_ports,curr_list,folder_name):
     for ssl_port in list_of_ssl_ports:
         # --color 0
         print ("[*]running testssl on " + ip_add_of_focus + " on port " + str(ssl_port))
-        cmd = "/root/Desktop/tools/scripts/testssl.sh/testssl.sh " +ip_add_of_focus+":"
+        if want_colour:
+            cmd = "/root/Desktop/tools/scripts/testssl.sh/testssl.sh --color 0 " +ip_add_of_focus+":"
+        else:
+            cmd = "/root/Desktop/tools/scripts/testssl.sh/testssl.sh " +ip_add_of_focus+":"
         cmd += str(ssl_port)
         name_of_output = folder_name+"/"+curr_list+"_testssl_"+ip_add_of_focus+"-"+str(ssl_port)
         run_command(cmd,name_of_output)
