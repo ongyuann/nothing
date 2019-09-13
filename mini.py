6ab4e332de3a7565a6be28920e398b497baa3b63
oya10 <32030081+oya10@users.noreply.github.com>
2019-09-13 16:02:28 +0800

    Update mini.py

--- mini.py
+++ mini.py
@@ -5,7 +5,7 @@ import sys,subprocess
 address = sys.argv[1].split(":")[0]
 port = sys.argv[1].split(":")[1]
 
-cmd = "nmap -v --script=http-* -p "+port+" "+address+" -oN http_"+address+":"+port
+cmd = "nmap -v -sV -sC --script=http-* -p "+port+" "+address+" -oN http_"+address+":"+port
 
 res = subprocess.call(cmd,shell=True)
 print (res)
